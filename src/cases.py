97b45d674fdfb1df21d70a124eb042a33cf957ab
Rafael M Mudafort <rafmudaf@gmail.com>
2019-06-25 11:03:42 -0600

    Add Todo’s in the case class

--- src/cases.py
+++ src/cases.py
@@ -7,28 +7,24 @@ class Case():
   def __init__(self, driver, case_directory, input_file):
     self.driver = driver
     self.case_directory = case_directory
-
-    # TODO: check that this directory exists
-    if (os.path.isdir(case_directory)):
-      print('Directory does exist')
-    else:
-      print('Directory does not exist')
-      # Maybe should also do more to prevent further errors?
-
-    # Input file is simply the template so 'bd_driver' not 'bd_driver.inp'
     self.input_file = input_file
 
-    # TODO: derive log file from input file or case directory
+    # TODO: how should this error be handled?
+    #  - you could create the directory but then you also have to get all the case files from somewhere
+    #  - or just bail and leave that up to the user
+    if not os.path.isdir(case_directory):
+      print('Directory does not exist')
+
     try:
       self.log_file = self.input_file.split('.')[0] + '.log'
     except:
       self.log_file = input_file + '.log'
 
+    # TODO: if it exists, maybe let the user know its being overwritten?
     if (os.path.exists(os.path.join(case_directory, self.log_file))):
       print('Log file does exist')
     else:
       print('Log file does not exist')
-      # Maybe should also do more to prevent further errors? 
   
   def run(self):
     stdout = sys.stdout  # if verbose else open(os.devnull, 'w')
