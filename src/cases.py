1b230b3b39ac8d7e1c8d902c39dd2b86764d7d00
Rafael M Mudafort <rafmudaf@gmail.com>
2019-06-26 16:22:40 -0600

    Cases cleanup and add todos

--- src/cases.py
+++ src/cases.py
@@ -5,59 +5,71 @@ from src.beamdyn_driver import BeamdynDriver
 from src.beamdyn_files import BeamdynPrimaryFile, BeamdynBladeFile, BeamdynInputFile, BeamdynInputSummaryFile
 
 class Case():
-  def __init__(self, driver, case_directory, input_file):
+  def __init__(self, driver, case_directory, input_files, primary_input_index=0):
+    """
+    driver: Driver - The Driver object which will be used to run this case
+    case_directory: Str - The location of this case
+    input_files: [Str] - A list of all the input files which describe this case
+    Optional primary_input_index: Int - The location in input_files of the primary input file
+    """
     self.driver = driver
     self.case_directory = case_directory
-    self.input_file = input_file
+    self.input_files = input_files
+    self.primary_input_file = self.input_files[primary_input_index]
 
     if not os.path.isdir(case_directory):
       print('Directory does not exist... Please setup the following: ' + case_directory)
+      # TODO: probably bail? otherwise, things will fail later on
 
+    # TODO: lets handle this better... use the case directory as the base name instead
     try:
-      self.log_file = self.input_file.split('.')[0] + '.log'
+      self.log_file = self.primary_input_file.split('.')[0] + '.log'
     except:
-      self.log_file = input_file + '.log'
+      self.log_file = self.primary_input_file + '.log'
 
     if (os.path.exists(os.path.join(case_directory, self.log_file))):
+      # TODO: Add a helper function
       print('Log file does exist... ')
       print('\tThis file will be overwritten now: ' + self.log_file)
-      print('\tThis file is located in: ' + self.case_directory)
-    else:
-      print('Log file does not exist')
   
   def run(self):
     stdout = sys.stdout  # if verbose else open(os.devnull, 'w')
     os.chdir(self.case_directory)
-    command = "{} {} > {}".format(self.driver.executable_path, self.input_file, self.log_file)
+    command = "{} {} > {}".format(self.driver.executable_path, self.primary_input_file, self.log_file)
     return subprocess.call(command, stdout=stdout, shell=True)
 
 class BeamdynCase(Case):
   def __init__(self, openfast_directory, case_type):
+    """
+    openfast_directory: Str - # TODO See example above
+    case_type: Str - # TODO See example above
+    """
+
     self.openfast_directory = openfast_directory
     self.case_type = case_type
-    # Set a common home directory
-    homedir = os.path.expanduser("~")
-    # Each case uses the same driver
-    driver = BeamdynDriver(homedir + '/openfast/build/modules/beamdyn/beamdyn_driver')
-    # Each case uses the same input file
-    input_file = 'bd_driver.inp'
-    # The location of the .log, .out, .ech, and .sum files
-    case_directory = self.openfast_directory + '/build/reg_tests/modules/beamdyn/' + self.case_type 
-    super().__init__(driver, case_directory, input_file)
+    driver = BeamdynDriver(self.openfast_directory + '/build/modules/beamdyn/beamdyn_driver')
+    input_files = [
+      'bd_driver.inp'
+      'bd_primary.inp'
+      'beam_props.inp'
+    ]
+    self.case_directory = self.openfast_directory + '/build/reg_tests/modules/beamdyn/' + self.case_type
+
+    super().__init__(driver, self.case_directory, input_files)
 
   # TODO: connect this with the beamdyn file classes
   #  - input files should use the yaml interface
   #  - output files should ultimatley be exported in yaml
 
   def import_to_yaml(self):
-    file_path = self.case_directory + '/bd_primary.inp'
-    temp_file = BeamdynPrimaryFile(file_path)
-    new_dict = temp_file.read()
-    temp_file.to_yaml(new_dict)
+    for input_file in self.input_files:
+      file_path = self.case_directory + input_file
+      temp_file = BeamdynPrimaryFile(file_path)
+      new_dict = temp_file.read()
+      temp_file.to_yaml(new_dict)
 
   # Convert the outputted summary file to YAML
   def export_to_yaml(self):
-    
     file_path = self.case_directory + '/bd_primary.inp.sum'
     temp_file = BeamdynInputSummaryFile(file_path)
     new_dict = temp_file.read()
