e3ba21131671883a6721bd5a69ce2c5dfb4d4640
Rafael M Mudafort <rafmudaf@gmail.com>
2019-06-28 15:13:25 -0600

    Add a script to convert openfast files to yaml

---
+++ convert_bd_to_yaml.py
@@ -0,0 +1,18 @@
+
+from src.beamdyn_files import BeamdynDriverFile
+from src.beamdyn_files import BeamdynPrimaryFile
+from src.beamdyn_files import BeamdynBladeFile
+
+# this script converts the input files for a BeamDyn driver case
+# into yaml files for use with this Python wrapper
+
+case_directory = "/Users/rmudafor/Development/openfast/reg_tests/r-test/modules/beamdyn/bd_isotropic_rollup/"
+bd_input_files = [
+    BeamdynDriverFile(case_directory + "bd_driver.inp"),
+    BeamdynPrimaryFile(case_directory + "bd_primary.inp"),
+    BeamdynBladeFile(case_directory + "beam_props.inp")
+]
+
+for f in bd_input_files:
+    f.init_output_file(f.filename + ".yml")
+    f.to_yaml(f._data)
