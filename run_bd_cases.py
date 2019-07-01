e9945bedff3ac157bfda890e2cc37c7f94aad02a
Rafael M Mudafort <rafmudaf@gmail.com>
2019-07-01 14:09:04 -0600

    Fill out the placeholders for the bd test script

--- run_bd_cases.py
+++ run_bd_cases.py
@@ -1,5 +1,5 @@
 import os.path
-from src.cases import BeamdynCase
+from src.beamdyn_cases import BeamdynCase
 from src.beamdyn_driver import BeamdynDriver
 
 # Set a common home directory
@@ -13,14 +13,14 @@ beamdyn_cases = [
     # BeamdynCase(openfast_directory, 'bd_5MW_dynamic'),
     # BeamdynCase(openfast_directory, 'bd_5MW_dynamic_gravity_Az00'),
     # BeamdynCase(openfast_directory, 'bd_5MW_dynamic_gravity_Az90'),
-    # BeamdynCase(openfast_directory, 'bd_curved_beam'),
-    BeamdynCase(openfast_directory, './bd_isotropic_rollup'),
+    BeamdynCase(openfast_directory, 'bd_curved_beam'),
+    # BeamdynCase(openfast_directory, 'bd_isotropic_rollup'),
     # BeamdynCase(openfast_directory, 'bd_static_cantilever_beam'),
     # BeamdynCase(openfast_directory, 'bd_static_twisted_with_k1')
 ]
 
 # Run each case
-for i,case in enumerate(beamdyn_cases):
+for i, case in enumerate(beamdyn_cases):
   # converts the yaml input files to openfast-style files
   case.initialize_input_files()
 
