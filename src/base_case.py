a3f521a59682ee3e8809aa0bd16c1efeff5d6736
Rafael M Mudafort <rafmudaf@gmail.com>
2019-07-01 12:39:14 -0600

    Split the cases module

---
+++ src/base_case.py
@@ -0,0 +1,39 @@
+import os
+import sys
+import subprocess
+
+
+class BaseCase():
+  def __init__(self, driver, case_directory, input_files, primary_input_index=0):
+    """
+    driver: Driver - The Driver object which will be used to run this case
+    case_directory: Str - The location of this case
+    input_files: [Str] - A list of all the input files which describe this case
+    Optional primary_input_index: Int - The location in input_files of the primary input file
+    """
+    self.driver = driver
+    self.case_directory = case_directory
+    self.input_files = input_files
+    self.primary_input_file = self.input_files[primary_input_index]
+
+    if not os.path.isdir(case_directory):
+      print('Directory does not exist... Please setup the following: ' + case_directory)
+      # TODO: probably bail? otherwise, things will fail later on
+
+    # TODO: lets handle this better... use the case directory as the base name instead
+    try:
+      self.log_file = self.primary_input_file.split('.')[0] + '.log'
+    except:
+      self.log_file = self.primary_input_file + '.log'
+
+    if (os.path.exists(os.path.join(case_directory, self.log_file))):
+      # TODO: Add a helper function
+      print('Log file does exist... ')
+      print('\tThis file will be overwritten now: ' + self.log_file)
+
+  def run(self):
+    stdout = sys.stdout  # if verbose else open(os.devnull, 'w')
+    os.chdir(self.case_directory)
+    command = "{} {} > {}".format(
+        self.driver.executable_path, self.primary_input_file, self.log_file)
+    return subprocess.call(command, stdout=stdout, shell=True)
--- src/beamdyn_cases.py
+++ src/beamdyn_cases.py
@@ -1,50 +1,15 @@
-import os
-import sys
-import subprocess
+from src.base_case import BaseCase
 from src.beamdyn_driver import BeamdynDriver
 from src.beamdyn_files import BeamdynPrimaryFile, BeamdynBladeFile, BeamdynInputFile, BeamdynInputSummaryFile
 from src.output_files import OutputPrimaryFile
 
 
-class Case():
-  def __init__(self, driver, case_directory, input_files, primary_input_index=0):
-    """
-    driver: Driver - The Driver object which will be used to run this case
-    case_directory: Str - The location of this case
-    input_files: [Str] - A list of all the input files which describe this case
-    Optional primary_input_index: Int - The location in input_files of the primary input file
-    """
-    self.driver = driver
-    self.case_directory = case_directory
-    self.input_files = input_files
-    self.primary_input_file = self.input_files[primary_input_index]
 
-    if not os.path.isdir(case_directory):
-      print('Directory does not exist... Please setup the following: ' + case_directory)
-      # TODO: probably bail? otherwise, things will fail later on
-
-    # TODO: lets handle this better... use the case directory as the base name instead
-    try:
-      self.log_file = self.primary_input_file.split('.')[0] + '.log'
-    except:
-      self.log_file = self.primary_input_file + '.log'
-
-    if (os.path.exists(os.path.join(case_directory, self.log_file))):
-      # TODO: Add a helper function
-      print('Log file does exist... ')
-      print('\tThis file will be overwritten now: ' + self.log_file)
-  
-  def run(self):
-    stdout = sys.stdout  # if verbose else open(os.devnull, 'w')
-    os.chdir(self.case_directory)
-    command = "{} {} > {}".format(self.driver.executable_path, self.primary_input_file, self.log_file)
-    return subprocess.call(command, stdout=stdout, shell=True)
-
-class BeamdynCase(Case):
+class BeamdynCase(BaseCase):
   def __init__(self, openfast_directory, case_directory):
     """
-    openfast_directory: Str - # TODO See example above
-    case_directory: Str - # TODO See example above
+    openfast_directory: Str - # TODO See example in base_case
+    case_directory: Str - # TODO See example in base_case
     """
 
     self.openfast_directory = openfast_directory
@@ -109,7 +74,7 @@ class BeamdynCase(Case):
     temp_file.to_text(file_string)
 
   def props_to_yaml(self):
-    file_path = self.case_directory + '/beam_props.inp' 
+    file_path = self.case_directory + '/beam_props.inp'
     temp_file = BeamdynBladeFile(file_path)
     new_dict = temp_file.read_t2y()
     temp_file.to_yaml(new_dict)   
@@ -118,4 +83,3 @@ class BeamdynCase(Case):
     file_path = self.case_directory + '/beam_props_inp.yml' 
     temp_file = BeamdynBladeFile(file_path)
     file_string = temp_file.read_y2t()
-    temp_file.to_text(file_string)
