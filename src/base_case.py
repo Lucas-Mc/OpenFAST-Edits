21f3a3b45ea7af569f0da0d02de23fdf0f08af9e
Rafael M Mudafort <rafmudaf@gmail.com>
2019-07-03 12:54:19 -0600

    Update the log file name

--- src/base_case.py
+++ src/base_case.py
@@ -12,7 +12,10 @@ class BaseCase():
     Optional primary_input_index: Int - The location in input_files of the primary input file
     """
     self.driver = driver
-    self.case_directory = case_directory
+
+    # the case directory should be of this form: <path_to>/<case_name>
+    self.case_directory = case_directory.rstrip(os.path.sep)
+
     self.input_files = input_files
     self.primary_input_file = self.input_files[primary_input_index]
 
@@ -24,19 +27,18 @@ class BaseCase():
       else:
         sys.exit(99)
     
+    # log file should be the name of the case directory + ".log"
+    self.log_file = os.path.basename(self.case_directory.split(os.path.sep)[-1]) + ".log"
+
     # store the current directory so we know where to return on end()
     self.calling_directory = os.getcwd()
-
-    parent_directory = os.path.dirname(self.case_directory)
-    base_name = os.path.basename(self.primary_input_file.filename) + ".log"
-    self.log_file =  os.path.join(parent_directory, base_name)
 
   def start(self):
     os.chdir(self.case_directory)
 
   def run(self, verbose=True):
     stdout = sys.stdout if verbose else open(os.devnull, 'w')
-    command = "{} {} # > {}".format(
+    command = "{} {} > {}".format(
       self.driver.executable_path,
       self.primary_input_file.openfast_filename,
       self.log_file
