a0f4eee12790999f39dffa81930e8699d4705e69
Lucas-Mc <lumccul3@gmail.com>
2019-07-09 14:15:46 -0600

    Added init unit test (not done)

--- tests/test_base_case.py
+++ tests/test_base_case.py
@@ -7,13 +7,22 @@ from src.beamdyn_files import BeamdynDriverFile
 
 class TestBaseCase(unittest.TestCase):
 
+  def test_init(self):
+
+    baseline_cd = os.path.dirname(os.path.realpath(__file__))
+    test_cd = os.path.join(baseline_cd,'more')
+    baseline_driver_path = os.path.join(baseline_cd,'test_driver')
+    baseline_driver = BeamdynDriver(baseline_driver_path)
+    baseline_if = [BeamdynDriverFile(baseline_cd, 'test_file.yml')]
+    baseline_case = BaseCase(baseline_driver,test_cd,baseline_if)
+    
+
   def test_start(self):
 
     baseline_cd = os.path.dirname(os.path.realpath(__file__))
-    # baseline_driver_path = os.path.join(baseline_cd,'beamdyn_driver')
-    baseline_driver_path = os.path.join(baseline_cd,'beamdyn_driver')
+    baseline_driver_path = os.path.join(baseline_cd,'test_driver')
     baseline_driver = BeamdynDriver(baseline_driver_path)
-    baseline_if = [BeamdynDriverFile(baseline_cd, 'bd_driver.yml')]
+    baseline_if = [BeamdynDriverFile(baseline_cd, 'test_file.yml')]
     baseline_case = BaseCase(baseline_driver,baseline_cd,baseline_if)
     
     test_loc = baseline_cd.rstrip(os.path.sep)
@@ -27,7 +36,7 @@ class TestBaseCase(unittest.TestCase):
     baseline_cd = os.path.dirname(os.path.realpath(__file__))
     baseline_driver_path = os.path.join(baseline_cd,'test_driver')
     baseline_driver = BeamdynDriver(baseline_driver_path)
-    baseline_if = [BeamdynDriverFile(baseline_cd, 'bd_driver.yml')]
+    baseline_if = [BeamdynDriverFile(baseline_cd, 'test_file.yml')]
     baseline_case = BaseCase(baseline_driver,baseline_cd,baseline_if)
     
     test_output = BaseCase.run(baseline_case)
@@ -37,9 +46,9 @@ class TestBaseCase(unittest.TestCase):
   def test_end(self):
 
     baseline_cd = os.path.dirname(os.path.realpath(__file__))
-    baseline_driver_path = os.path.join(baseline_cd,'beamdyn_driver')
+    baseline_driver_path = os.path.join(baseline_cd,'test_driver')
     baseline_driver = BeamdynDriver(baseline_driver_path)
-    baseline_if = [BeamdynDriverFile(baseline_cd, 'bd_driver.yml')]
+    baseline_if = [BeamdynDriverFile(baseline_cd, 'test_file.yml')]
     baseline_case = BaseCase(baseline_driver,baseline_cd,baseline_if)
     
     cd_before = os.getcwd()
