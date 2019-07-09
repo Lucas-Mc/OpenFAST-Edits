f8a0fb7f743ae80e4c1d019f8b1d7d3723d0a806
Lucas-Mc <lumccul3@gmail.com>
2019-07-09 13:16:18 -0600

    Added new driver to unit test

--- tests/test_base_case.py
+++ tests/test_base_case.py
@@ -10,13 +10,13 @@ class TestBaseCase(unittest.TestCase):
   def test_start(self):
 
     baseline_cd = os.path.dirname(os.path.realpath(__file__))
+    # baseline_driver_path = os.path.join(baseline_cd,'beamdyn_driver')
     baseline_driver_path = os.path.join(baseline_cd,'beamdyn_driver')
     baseline_driver = BeamdynDriver(baseline_driver_path)
     baseline_if = [BeamdynDriverFile(baseline_cd, 'bd_driver.yml')]
     baseline_case = BaseCase(baseline_driver,baseline_cd,baseline_if)
     
     new_dir = os.path.abspath(os.path.join(baseline_cd, os.pardir))
-    # os.chdir(baseline_cd.replace('/tests',''))
     os.chdir(new_dir)
     cd_before = os.getcwd()
     BaseCase.end(baseline_case)
@@ -28,7 +28,7 @@ class TestBaseCase(unittest.TestCase):
   def test_run(self):
 
     baseline_cd = os.path.dirname(os.path.realpath(__file__))
-    baseline_driver_path = os.path.join(baseline_cd,'beamdyn_driver')
+    baseline_driver_path = os.path.join(baseline_cd,'test_driver')
     baseline_driver = BeamdynDriver(baseline_driver_path)
     baseline_if = [BeamdynDriverFile(baseline_cd, 'bd_driver.yml')]
     baseline_case = BaseCase(baseline_driver,baseline_cd,baseline_if)
@@ -46,7 +46,6 @@ class TestBaseCase(unittest.TestCase):
     baseline_case = BaseCase(baseline_driver,baseline_cd,baseline_if)
     
     new_dir = os.path.abspath(os.path.join(baseline_cd, os.pardir))
-    # os.chdir(baseline_cd.replace('/tests',''))
     os.chdir(new_dir)
     cd_before = os.getcwd()
     BaseCase.end(baseline_case)
@@ -57,8 +56,9 @@ class TestBaseCase(unittest.TestCase):
 
 def tearDownModule():
     # Clean up and remove all temporary files
-    os.remove('bd_primary.inp.ech')
-    os.remove('bd_primary.inp.sum')
+    #pass
+    #os.remove('bd_primary.inp.ech')
+    #os.remove('bd_primary.inp.sum')
     os.remove('tests.log')
 
 
