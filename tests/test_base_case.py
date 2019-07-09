efe697b40829d0c4c9e1c3bc2472e34f9186355d
Lucas-Mc <lumccul3@gmail.com>
2019-07-09 13:39:59 -0600

    Fixed Travis CI bugs again

--- tests/test_base_case.py
+++ tests/test_base_case.py
@@ -50,7 +50,8 @@ class TestBaseCase(unittest.TestCase):
 
 def tearDownModule():
     # Clean up and remove all temporary files
-    os.remove('tests.log')
+    baseline_cd = os.path.dirname(os.path.realpath(__file__))
+    os.remove(os.path.join(baseline_cd,'tests.log'))
 
 
 if __name__ == '__main__':
